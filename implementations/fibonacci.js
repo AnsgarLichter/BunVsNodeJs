8cd97ede8f9f551c319f91d16efb497476c272e3
Ansgar Lichter <lichteransgar@gmail.com>
2023-10-21 16:10:19 +0200

    feat(implementations): fibonacci

---
+++ implementations/fibonacci.js
@@ -0,0 +1,13 @@
+const fibonacci = (number) => {
+    if (number <= 0) {
+        return 0;
+    } else if (number <= 1) {
+        return 1;
+    } else if (number <= 2) {
+        return 2;
+    }
+
+    return fibonacci(number-1) + fibonacci(number-2);
+};
+
+console.log(fibonacci(40));
